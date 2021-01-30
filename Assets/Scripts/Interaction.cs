8fe4e6767249ce85c3172294fee66ca63d2fd012
huulong <n.huu.long@gmail.com>
2021-01-30 22:51:19 +0100

    [ROOM] Imported package containing Room data from older version of Unity

---
+++ Assets/Scripts/Interaction.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+
+
+    public GameObject PCInteractions;
+    public GameObject BedInteractions;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PCInteractions.SetActive(false);
+        BedInteractions.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (PlayerController.Instance.IsOnPC == true)
+        {
+            PCInteractions.SetActive(true);
+         
+        }
+        else
+        {
+            PCInteractions.SetActive(false);
+
+        }
+
+        if (PlayerController.Instance.IsOnBed == true)
+        {
+            BedInteractions.SetActive(true);
+       
+        }
+        else
+        {
+            BedInteractions.SetActive(false);
+        }
+
+
+
+
+
+
+
+    }
+}
