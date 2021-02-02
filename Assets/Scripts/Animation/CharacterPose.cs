dc58a186adf027557089fa98254791c3732c1ac1
huulong <n.huu.long@gmail.com>
2021-02-02 21:06:59 +0100

    [ANIMATION] Added CharacterPose script to control material alpha based on public alpha member Each animation must set alpha to 0 or 1 and Animator Graph transitions will handle fading Only tested for Having a Drink pose

---
+++ Assets/Scripts/Animation/CharacterPose.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using CommonsHelper;
+using UnityEngine;
+
+public class CharacterPose : MonoBehaviour
+{
+    [Tooltip("List of renderers for which material instances should be faded in/out during pose transitions")]
+    public Renderer[] renderers;
+
+    [Tooltip("Fading alpha, must be set to 0/1 by animation for pose transition (let Animator Graph handle transition)")]
+    public float alpha;
+
+    /// Track last alpha to only change material color when dirty
+    private float m_LastAlpha;
+
+    private void Awake()
+    {
+        // material instance should be visible on start
+        m_LastAlpha = 1f;
+    }
+    
+    private void Update()
+    {
+        if (m_LastAlpha != alpha)
+        {
+            m_LastAlpha = alpha;
+        
+            foreach (Renderer renderer in renderers)
+            {
+                // we assume single material per renderer
+                // if adding more, iterate on materials instead
+                renderer.material.color = renderer.material.color.ToAlpha(alpha);
+            }
+        }
+    }
+}
